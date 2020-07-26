8bf02aedad63c6c9c5f3f66469636ee52f602a6a
leo2409 <leonardo.brunetti24@gmail.com>
2020-07-26 11:56:18 +0200

    admin show film in the db

--- templates/adform.html.php
+++ templates/adform.html.php
@@ -1,8 +1,8 @@
-
+<h1>Films</h1>
 <div class="film-form">
+    <h2>Aggiungi film</h2>
     <form action="ADAG.php" method="post" enctype="multipart/form-data">
-        <div class="film">
-            <h2>Film</h2>
+        <div class="film-campi">
             <section class="campo">
                 <label for="title">Titolo</label>
                 <input type="text" id="titolo" name="film[titolo]">
@@ -13,10 +13,40 @@
             </section>
             <section class="campo">
                 <label for="descrizione">Descrizione</label>
-                <textarea name="film[descrizione]" id="descrizione" cols="30" rows="3"></textarea>
+                <textarea id="descrizione" name="film[descrizione]" cols="30" rows="3"></textarea>
             </section>
-            
         </div>
         <input type="submit" value="inserisci">
     </form>
+</div>
+<div class="film-container">
+
+    <?php foreach($films as $film) : ?>
+        <div class="film">
+            <div class="locandina-container">
+                <img src="<?=$film['locandina']?>" alt="<?php echo 'locandina ' . $film['title'] ?>">
+            </div>
+            <div class="descrizione">
+                <h2>
+                    <?=$film['titolo']?>
+                </h2>
+                <p>
+                    <?=$film['descrizione']?>
+                </p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+</div>
+
+<h1>Eventi</h1>
+<div class="event-container">
+    <div class="event-form">
+        <h2>Aggiungi evento</h2>
+        <form action="ADAG.php" method="post">
+            <section class="campo">
+                <label for=""></label>
+            </section>
+        </form>
+    </div>
 </div>
