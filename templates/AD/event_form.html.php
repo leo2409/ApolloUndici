a9a2c64b116df9f2fcbb4b4258f8954c032e7914
leo2409 <leonardo.brunetti24@gmail.com>
2020-07-30 10:42:04 +0200

    bug modifica eventi

--- templates/AD/event_form.html.php
+++ templates/AD/event_form.html.php
@@ -1,35 +1,38 @@
 
 <div class="event-form">
     <div class="event-stuff">
-        <img src="<?=$film['locandina'] ?>" alt="locandina" class="locandina-preview">
-        <input type="hidden" name="evento[id_film]" value="<?=$film['id_film']?>">
-        <section class="campo">
-            <label for="date">data</label>
-            <input type="date" id="date" name="evento[data]" value="<?=$evento['data'] ?? '' ?>">
-        </section>
-        <section class="campo">
-            <label for="orario">orario</label>
-            <input type="time" id="orario" name="evento[orario]" value="<?=$evento['orario'] ?? '' ?>">
-        </section>
-        <section class="campo">
-            <label for="descrizione">descrizione</label>
-            <textarea id="descrizione" name="evento[descrizione]" rows="20"><?=$evento['descrizione'] ?? $film['descrizione']?></textarea>
-        </section>
-        <section class="campo">
-            <label for="posti">posti</label>
-            <input type="number" id="posti" value="<?=$evento['posti'] ?? '95' ?>" name="evento[posti]">
-        </section>
+        <form action="ADAG.php?route=event/edit" method="post" id="form-event">
+            <img src="<?=$film['locandina'] ?>" alt="locandina" class="locandina-preview">
+            <input type="hidden" name="evento[id_evento]" value="<?=$evento['id_evento'] ?? '' ?>">
+            <input type="hidden" name="evento[id_film]" value="<?=$film['id_film']?>">
+            <section class="campo">
+                <label for="date">data</label>
+                <input type="date" id="date" name="evento[data]" value="<?=$evento['data'] ?? '' ?>">
+            </section>
+            <section class="campo">
+                <label for="orario">orario</label>
+                <input type="time" id="orario" name="evento[orario]" value="<?=$evento['orario'] ?? '' ?>">
+            </section>
+            <section class="campo">
+                <label for="descrizione">descrizione</label>
+                <textarea id="descrizione" name="evento[descrizione]" rows="20"><?=$evento['descrizione'] ?? $film['descrizione']?></textarea>
+            </section>
+            <section class="campo">
+                <label for="posti">posti</label>
+                <input type="number" id="posti" value="<?=$evento['posti'] ?? '95' ?>" name="evento[posti]">
+            </section>
 
-        <!--
-            TIPOLOGIA DI EVENTO
-        <section class="campo">
-            <label for="presentazione">presentazione</label>
-            <input type="radio" id="presentazione" value="presentazione" name="tipologia">
-            <label for="presentazione-libro">presentazione libro</label>
-            <input type="radio" id="presentazione-libro" value="presentazione-libro" name="tipologia">
-        </section>
-        -->
+            <!--
+                TIPOLOGIA DI EVENTO
+            <section class="campo">
+                <label for="presentazione">presentazione</label>
+                <input type="radio" id="presentazione" value="presentazione" name="tipologia">
+                <label for="presentazione-libro">presentazione libro</label>
+                <input type="radio" id="presentazione-libro" value="presentazione-libro" name="tipologia">
+            </section>
+            -->
 
-        <input type="submit" value="inserisci">
+            <input type="submit" value="inserisci">
+        </form>
     </div>
 </div>
