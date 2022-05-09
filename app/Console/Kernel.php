7048f72e142fde9a8e7c374605f94c935a20f290
leo <leonardo.brunetti24@gmail.com>
2022-05-09 13:52:39 +0200

    trailer e director

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sitemap:generate')->everyMinute();
+        $schedule->command('sitemap:generate')->daily();
     }
 
     /**
