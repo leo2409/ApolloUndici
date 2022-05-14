f5c62ccb7d311d193e2e3547b4646c572359bfe1
leo <leonardo.brunetti24@gmail.com>
2022-05-14 04:19:32 +0200

    automatic database backup

---
+++ app/Console/Commands/DatabaseBackUp.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Storage;
+
+class DatabaseBackUp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a backup of the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $dir = Carbon::now()->format('M_Y');
+        if (!Storage::exists("backup/{$dir}")) {
+            Storage::makeDirectory("backup/{$dir}");
+        }
+
+        $filename = "backup-" . Carbon::now()->format('d-m-Y') . ".gz";
+
+        $command = "mysqldump --user=" . config('database.connections.mysql.username') ." --password=" . config('database.connections.mysql.password') . " --host=" . config('database.connections.mysql.host') . " " . config('database.connections.mysql.database') . "  | gzip > " . storage_path() . "/app/backup/{$dir}/" . $filename;
+
+        $returnVar = NULL;
+        $output  = NULL;
+
+        exec($command, $output, $returnVar);
+    }
+}
